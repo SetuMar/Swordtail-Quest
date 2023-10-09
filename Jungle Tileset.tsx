ed25264e3f7864febd93d7519f56555798dd32fb
Huamin Lu <146397352+DirectMaker@users.noreply.github.com>
2023-10-09 14:24:57 -0400

    Update Jungle Tileset.tsx

--- Jungle Tileset.tsx
+++ Jungle Tileset.tsx
@@ -1,4 +1,1 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Jungle Tileset" tilewidth="16" tileheight="16" tilecount="80" columns="8">
- <image source="../OneDrive - MSFT/Pygame Assets/Jungle/Jungle Tileset.png" width="128" height="160"/>
-</tileset>
+
