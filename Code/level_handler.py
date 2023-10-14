6ca818d53f326d7be02f4c2edc8c26a4f48d9714
bobzheng <shenyanzheng00@gmail.com>
2023-10-14 17:32:44 -0400

    Set relative path for the Graphics folder (fix missing file error on Windows)

--- Code/level_handler.py
+++ Code/level_handler.py
@@ -1,4 +1,5 @@
 from pytmx.util_pygame import load_pygame
+import pathlib as pl
 import pygame
 # import pygame and pytmx
 
@@ -9,51 +10,57 @@ from settings import *
 
 import powerups
 
+# create the relative path of the Graphics folder
+current_file = pl.Path(__file__)
+parent_directory = current_file.parent.parent
+graphics_path = pl.Path(parent_directory / 'Graphics')
+
+
 class GameOverHandler:
     def __init__(self) -> None:
         self.current_level_completed = False
         self.level_number = 1
-        
-        self.current_level = r"Graphics/Levels/l1.tmx"
-        
+
+        self.current_level = str(graphics_path / 'Levels' / 'l1.tmx')
+
     def complete_level(self, tiles, player):
         self.level_number += 1
-        self.current_level = f'Graphics/Levels/l{self.level_number}.tmx'
+        self.current_level = str(graphics_path / 'Levels' / f'l{self.level_number}.tmx')
         tiles, player.rect.topleft = self.generate_level()
-        
+
         self.current_level_completed = False
-    
+
         return tiles
-    
+
     def restart_level(self, tiles, player):
-        self.current_level = f'Graphics/Levels/l{self.level_number}.tmx'
+        self.current_level = str(graphics_path / 'Levels' / f'l{self.level_number}.tmx')
         tiles, player.rect.topleft = self.generate_level()
-        
+
         player.health = 1
         player.direction = pygame.Vector2(0, 0)
-        
+
         self.current_level_completed = False
-        
+
         return tiles
-    
+
     def generate_level(self):
         tmx_data = load_pygame(self.current_level)
         # get the data for a specific level
-        
+
         tiles = {}
         # list of all tiles
-        
+
         player_position = None
-        
+
         for layer in tmx_data.visible_layers:
             # go through all visible layers
-            
+
             layer_tiles = []
-            
+
             for tile_data in layer.tiles():
                 image = tile_data[2]
                 position = pygame.Vector2(tile_data[0] * BLOCK_SIZE, tile_data[1] * BLOCK_SIZE)
-                if layer.name in powerups.Powerup.powerup_layer_names: 
+                if layer.name in powerups.Powerup.powerup_layer_names:
                     layer_tiles.append(powerups.Powerup(image, position, layer.name))
                 elif layer.name == "player":
                     player_position = position
@@ -62,8 +69,8 @@ class GameOverHandler:
                 else:
                     layer_tiles.append(block.Tile(image, position))
             # add tile to list of tiles
-            
-            tiles.update({layer.name:layer_tiles})
-                
+
+            tiles.update({layer.name: layer_tiles})
+
         return tiles, player_position
         # return list of tiles and player position
